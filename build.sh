980cf5c9af398c6d3487f4152b806dbb8e16737c
kerta1n <kroland705@gmail.com>
2022-11-10 18:56:09 +0000

    Upload New File

---
+++ build.sh
@@ -0,0 +1,9 @@
+docker compose down
+rm -f *.mod
+rm -f *.sum
+touch argvMatey
+docker run --rm -it -v ${PWD}/containerBuild.sh:/containerBuild.sh -v ${PWD}/argvMatey.go:/argvMatey.go -v ${PWD}/argvMatey:/argvMatey golang bash /containerBuild.sh
+chmod +x argvMatey
+docker network create --subnet=172.18.0.0/16 workwithvpn
+sh containerBuild.sh
+
